c75b03ff649aeade7346e443831ef75f20b1ba50
luisatencio <luis.atencio@citrix.com>
2015-07-15 11:11:29 -0400

    Spiking with bars and graphs

---
+++ public/javascripts/spike.js
@@ -0,0 +1,10 @@
+require(['d3'], function(d3) {
+
+    var data = [4, 8, 15, 16, 23, 42];
+    d3.select(".chart")
+        .selectAll("div")
+        .data(data)
+        .enter().append("div")
+        .style("width", function(d) { return d * 10 + "px"; })
+        .text(function(d) { return d; });
+});
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'PipeViz' });
 });
 
+/* GET spike page. */
+router.get('/spike', function(req, res, next) {
+  res.render('spike', { title: 'PipeViz' });
+});
 module.exports = router;
