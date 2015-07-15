3bb67fc93251cf884ef9e69f9c3df49155d71676
luisatencio <luis.atencio@citrix.com>
2015-07-15 11:38:54 -0400

    Spiking with bars and graphs

--- public/javascripts/spike.js
+++ public/javascripts/spike.js
@@ -1,10 +1,42 @@
 require(['d3'], function(d3) {
-
     var data = [4, 8, 15, 16, 23, 42];
-    d3.select(".chart")
-        .selectAll("div")
+
+    stages = {
+        'DEV' : 25,
+        'QA'  : 50,
+        'UAT' : 75,
+        'PROD': 100
+    };
+
+    var data = [
+        {name: "1.0.0",    value:  'DEV'},
+        {name: "2.0.0",    value:  'PROD'}
+    ];
+
+    var width = 420,
+        barHeight = 20;
+
+    var x = d3.scale.linear()
+        .domain([0, d3.max(data, function(d) { return stages[d.value]; })])
+        .range([0, width]);
+
+    var chart = d3.select(".chart")
+        .attr("width", width)
+        .attr("height", barHeight * data.length);
+
+    var bar = chart.selectAll("g")
         .data(data)
-        .enter().append("div")
-        .style("width", function(d) { return d * 10 + "px"; })
-        .text(function(d) { return d; });
+        .enter().append("g")
+        .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+    bar.append("rect")
+        .attr("width", function(d) { return x(stages[d.value]); })
+        .attr("height", barHeight - 1);
+
+    bar.append("text")
+        .attr("x", function(d) { return x(stages[d.value]) - 3; })
+        .attr("y", barHeight / 2)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.name; });
+
 });
